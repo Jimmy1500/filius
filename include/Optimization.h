afe152a36b59a29d698c8c5a0ac1f488eb4bfa3b
James Ding <james.ding.illinois@gmail.com>
2018-10-21 14:05:08 -0500

    Project Filius: calibrator api bug fixes

--- include/Optimization.h
+++ include/Optimization.h
@@ -30,9 +30,9 @@ class Optimization{
         Optimization();
         ~Optimization();
         
-        void calibrate(RateModel*, RateInstrument*, size_t, double*, size_t, double, double k=0.01, size_t loss_trials=1);
-        void measureGradient(double*, size_t, RateModel * model, RateInstrument *, size_t, double*);
+        void calibrate (RateModel*, RateInstrument*, size_t, double*, size_t, double, double k=0.01, size_t loss_trials=1);
+        void getGradient (double*, size_t, RateModel * model, RateInstrument *, size_t, double*);
         double loss_function (RateInstrument * instruments, size_t num_instrs, double * weights, size_t order = 2);
-        double avg_loss (G2PP * model, RateInstrument * instruments, size_t num_intrs, double * weights, int ntrials, int order = 2);
+        double avg_loss (G2PP * model, RateInstrument * instruments, size_t num_intrs, double * weights, int ntrials, size_t order = 2);
 };
 #endif
--- src/Optimization.cpp
+++ src/Optimization.cpp
@@ -9,15 +9,15 @@ Optimization::~Optimization(){
 }
 
 
-void calibrate(RateModel* model, RateInstrument* instruments, size_t num_instrs, double* weights, size_t max_iter, double precision, double k, size_t loss_trials){
+void Optimization::calibrate (RateModel* model, RateInstrument* instruments, size_t num_instrs, double* weights, size_t max_iter, double precision, double k, size_t loss_trials){
 
 }
 
-void measureGradient(double * gradient, size_t num_params, RateModel * model, RateInstrument * instruments, size_t num_instrs, double * weights){
+void Optimization::getGradient (double * gradient, size_t num_params, RateModel * model, RateInstrument * instruments, size_t num_instrs, double * weights){
 
 }
 
-double loss_function (RateInstrument * instruments, size_t num_instrs, double * weights, int order = 2){
+double Optimization::loss_function (RateInstrument * instruments, size_t num_instrs, double * weights, size_t order){
     double loss = 0;
     size_t i;
     for ( i = 0; i < num_instrs; ++i){
@@ -26,7 +26,7 @@ double loss_function (RateInstrument * instruments, size_t num_instrs, double * 
     return loss;
 }
 
-double avg_loss (G2PP * model, RateInstrument * instruments, size_t num_instrs, double * weights, int ntrials, int order = 2){
+double Optimization::avg_loss (G2PP * model, RateInstrument * instruments, size_t num_instrs, double * weights, int ntrials, size_t order){
     size_t i;
     double avg = 0;
     for ( i = 0; i < num_instrs; ++i){
