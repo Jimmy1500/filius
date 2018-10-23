268adc5c6705550e047d4e0f2be92a07bf2ef121
James Ding <james.ding.illinois@gmail.com>
2018-10-22 23:48:18 -0500

    Project Filius: Calibration method update

--- include/Optimization.h
+++ include/Optimization.h
@@ -23,6 +23,11 @@
 #include "G2PP.h"
 #include "Swaption.h"
 
+#define DEEPCOPY_ARRAY(target, value, length)   \
+        for (i = 0; i < length; ++i){           \
+            target[i] = value[i];               \
+        }
+
 class Optimization{
     private:
         Rand<double> * Generator;
@@ -30,9 +35,9 @@ class Optimization{
         Optimization();
         ~Optimization();
         
-        void calibrate (RateModel *, RateInstrument *, size_t, double *, size_t, double, double k=0.01, size_t loss_trials=1);
-        void getGradient (double *, size_t, RateModel *, RateInstrument *, size_t, double *);
-        double loss_function (RateInstrument * , size_t , double * , size_t order = 2);
-        double avg_loss (RateModel * , RateInstrument * , size_t , double * , size_t , size_t order = 2);
+        void calibrate (RateModel *, RateInstrument *, size_t, double *, size_t, double, double k=0.01, double alpha=0.5, size_t loss_trials=3);
+        void getGradient (double *, size_t *, size_t, RateModel *, RateInstrument *, double *, size_t);
+        double loss_function (RateInstrument *, double *, size_t,  size_t order = 2);
+        double avg_loss (RateModel *, RateInstrument *, double *, size_t,  size_t , size_t order = 2);
 };
 #endif
--- src/Optimization.cpp
+++ src/Optimization.cpp
@@ -9,29 +9,106 @@ Optimization::~Optimization(){
 }
 
 
-void Optimization::calibrate (RateModel* model, RateInstrument* instruments, size_t num_instrs, double* weights, size_t max_iter, double precision, double k, size_t loss_trials){
+void Optimization::calibrate (RateModel* model, RateInstrument* instrs, size_t num_instrs, double* weights, size_t max_iter, double precision, double k, double alpha, size_t num_trials){
+    if ( precision <= 0.0 ) {
+        precision = 0.000000000000000000001;
+#ifdef __DEBUG__
+        DEBUG("precision must be greater than 0")
+#endif
+    }
+    if ( k <= 0.0 ) {
+        k = 0.000000000000000000001;
+#ifdef __DEBUG__
+        DEBUG("k must be greater than 0")
+#endif
+    }
+    if ( alpha <= 0.0 ) {
+        precision = 0.000000000000000000001;
+#ifdef __DEBUG__
+        DEBUG("alpha must be greater than 0")
+#endif
+    }
 
+    switch (model->getModelType()){
+        case RMT_G2PP:
+            if ( G2PP * g2pp = dynamic_cast<G2PP *>(model) ){
+                size_t i; 
+                const size_t num_params = 5; size_t keys[num_params] = {G2::A, G2::B, G2::SIGMA_1, G2::SIGMA_2, G2::RHO};
+                double curr_guess[num_params] = {0., 0., 0., 0., 0.};
+                double next_guess[num_params] = {0., 0., 0., 0., 0.};
+                double gradient[num_params] = {0., 0., 0., 0., 0.};
+
+                Generator = new Rand<double>((num_params+1),1,0,1);
+                size_t iter = 0;
+                double factor = precision;  //improvement measure
+                double prob = 0.;           // neighboring state transition probability
+                double initial_temp = avg_loss(g2pp, instrs, weights, num_instrs, num_trials);
+                double curr_temp = initial_temp, next_temp = initial_temp;
+
+                g2pp->getParameters(keys, curr_guess, num_params);
+                DEEPCOPY_ARRAY(next_guess, curr_guess, num_params);
+                do{
+                    if ( !curr_temp ){
+                        curr_temp = avg_loss(g2pp, instrs, weights, num_instrs, num_trials);
+                    }
+                    else if ( curr_temp <= precision ) {
+                        break; //accept current state
+                    } 
+
+                    getGradient(gradient, keys, num_params, model, instrs, weights, num_instrs);
+                    for (i = 0; i < num_params; ++i){ next_guess[i] += -alpha * gradient[i] + Generator->nrand(0, i); }
+
+                    g2pp->setParameters(keys, next_guess, num_params);
+                    next_temp = avg_loss(g2pp, instrs, weights, num_instrs, num_trials);
+
+                    if ( isnan(next_temp) ){ continue; }
+                    else if ( next_temp <= precision ) {
+                        curr_temp = next_temp;
+                        DEEPCOPY_ARRAY(curr_guess, next_guess, num_params)
+                        break; //accept new state
+                    }else if ( next_temp < curr_temp ){
+                        curr_temp = next_temp;
+                        DEEPCOPY_ARRAY(curr_guess, next_guess, num_params)
+                        factor = curr_temp/initial_temp;
+                    }else {
+                        prob = exp( (curr_temp - next_temp) / (k * curr_temp) );
+                        if ( Generator->urand(0, num_params) <= prob ){
+                            curr_temp = next_temp;
+                            DEEPCOPY_ARRAY(curr_guess, next_guess, num_params)
+                        }else{
+                            g2pp->setParameters(keys, curr_guess, num_params);
+                            DEEPCOPY_ARRAY(next_guess, curr_guess, num_params)
+                        }
+                    }
+                }while ( iter < max_iter && factor >= precision );
+            }
+            break;
+        case RMT_BLACK:
+            break;
+        default:
+            break;
+    }
 }
 
-void Optimization::getGradient (double * gradient, size_t num_params, RateModel * model, RateInstrument * instruments, size_t num_instrs, double * weights){
+void Optimization::getGradient (double * gradient, size_t * param_keys, size_t num_params, RateModel * model, RateInstrument * instrs, double * weights, size_t num_instrs){
     double f_left, f_right;
-    double delta = 0.0001;
+    double delta = 0.00001;
 
     size_t i;
     switch (model->getModelType()){
         case RMT_G2PP:
             if ( G2PP * g2pp = dynamic_cast<G2PP *>(model) ){
-                double param_i;
+                double param;
                 for ( i = 0; i < num_params; ++i){
-                    param_i = g2pp->getParameter(i);
-                    g2pp->setParameter(i, (param_i-delta));
-                    f_left = loss_function(instruments, num_instrs, weights);
+                    param = g2pp->getParameter(param_keys[i]);
+                    g2pp->setParameter(param_keys[i], (param-delta));
+                    f_left = loss_function(instrs, weights, num_instrs);
 
-                    g2pp->setParameter(i, (param_i+delta));
-                    f_right = loss_function(instruments, num_instrs, weights);
+                    g2pp->setParameter(param_keys[i], (param+delta));
+                    f_right = loss_function(instrs, weights, num_instrs);
 
                     gradient[i] = (f_right - f_left) / delta*0.5;
-                    g2pp->setParameter(i, param_i);
+                    g2pp->setParameter(param_keys[i], param);
                 }
             }
             break;
@@ -42,21 +119,21 @@ void Optimization::getGradient (double * gradient, size_t num_params, RateModel 
     }
 }
 
-double Optimization::loss_function (RateInstrument * instruments, size_t num_instrs, double * weights, size_t order){
+double Optimization::loss_function (RateInstrument * instrs, double * weights, size_t num_instrs, size_t order){
     double loss = 0;
     size_t i;
     for ( i = 0; i < num_instrs; ++i){
-        loss += weights[i] * instruments[i].getLoss(order);
+        loss += weights[i] * instrs[i].getLoss(order);
     }
     return loss;
 }
 
-double Optimization::avg_loss (RateModel * model, RateInstrument * instruments, size_t num_instrs, double * weights, size_t num_trials, size_t order){
+double Optimization::avg_loss (RateModel * model, RateInstrument * instrs, double * weights, size_t num_instrs, size_t num_trials, size_t order){
     size_t i;
     double avg = 0;
     for ( i = 0; i < num_trials; ++i){
         model->markDirtyAll(); // ensure simulation process kicks in to recalculate
-        avg += fabs(loss_function(instruments, num_instrs, weights, order));
+        avg += fabs(loss_function(instrs, weights, num_instrs, order));
     }
     return (avg/(double)num_trials);
 }
