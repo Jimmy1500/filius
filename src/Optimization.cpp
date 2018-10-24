e85f980dd49f916d1ae11654d5e764dbf33cacaa
James Ding <james.ding.illinois@gmail.com>
2018-10-23 23:30:24 -0500

    Project Filius: calibration default hyperparamter adjustment

--- src/Optimization.cpp
+++ src/Optimization.cpp
@@ -11,7 +11,7 @@ Optimization::~Optimization(){
 
 void Optimization::calibrate (RateModel* model, RateInstrument* instrs, size_t num_instrs, double* weights, size_t max_iter, double precision, double k, double alpha, size_t num_trials){
     if ( precision <= 0.0 ) {
-        precision = 1.e-17;
+        precision = 1.e-12;
 #ifdef __DEBUG__
         DEBUG("precision must be greater than 0")
 #endif
